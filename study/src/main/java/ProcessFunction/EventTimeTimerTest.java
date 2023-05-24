b415b8eb5a634dc5dadcc7c26e512975b9f98f5d
LiuXPeng <703762445@qq.com>
2023-05-24 19:54:33 +0800

    84

--- study/src/main/java/ProcessFunction/EventTimeTimerTest.java
+++ study/src/main/java/ProcessFunction/EventTimeTimerTest.java
@@ -6,6 +6,7 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.util.Collector;
 import source.ClickSource;
 import source.Event;
@@ -28,7 +29,7 @@ public class EventTimeTimerTest {
         env.setParallelism(1);
 //        DataStreamSource<Event> source = env.addSource(new ClickSource());
 
-        SingleOutputStreamOperator<Event> stream = env.addSource(new ClickSource())
+        SingleOutputStreamOperator<Event> stream = env.addSource(new CustomSource())
                 .assignTimestampsAndWatermarks(WatermarkStrategy.<Event>forBoundedOutOfOrderness(Duration.ZERO)
                         .withTimestampAssigner(new SerializableTimestampAssigner<Event>() {
                             @Override
@@ -47,10 +48,28 @@ public class EventTimeTimerTest {
 
                     @Override
                     public void onTimer(long timestamp, KeyedProcessFunction<String, Event, String>.OnTimerContext ctx, Collector<String> out) throws Exception {
-                        out.collect("定时器触发，触发时间：" + new Timestamp(timestamp));
+                        out.collect("定时器触发，触发时间：" + new Timestamp(timestamp)+",watermark:" + ctx.timerService().currentWatermark());
                     }
                 }).print();
 
         env.execute();
+    }
+
+
+    public static class CustomSource implements SourceFunction<Event> {
+        @Override
+        public void run(SourceContext<Event> ctx) throws Exception {
+            ctx.collect(new Event("Mary", "/home", 1000L));
+            Thread.sleep(5000L);
+            ctx.collect(new Event("Alice", "/home", 11000L));
+            Thread.sleep(5000L);
+            ctx.collect(new Event("Bob", "/home", 11001L));
+            Thread.sleep(5000L);
+        }
+
+        @Override
+        public void cancel() {
+
+        }
     }
 }
